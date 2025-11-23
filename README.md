3ace3055578881b87637456386e1aebec2e44078
Chantelle Bradley <info@starlette.co.za>
2025-11-23 17:35:45 +0200

    Config updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Installation
 
 1. Run `composer install`
-2. 
+2. Update DB credentials, if necessary, here: `partials/config.php`
--- partials/config.php
+++ partials/config.php
@@ -1,10 +1,9 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+/* Database credentials */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'clockfie');
+define('DB_NAME', 'knockom5e9r9_martial_arts_gym');
+define('DB_USERNAME', 'knockom5e9r9_elite_gym');
+define('DB_PASSWORD', '[6k9B0v$%tuhM£U@@^£p$a<vh_CTiVi&]FcZsH[');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -14,8 +13,9 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$gmailid = ''; // YOUR gmail email
-$gmailpassword = ''; // YOUR gmail password
-$gmailusername = ''; // YOUR gmail User name
+/* Gmail SMTP credentials */
+$gmailid = 'knockoutnationselite@gmail.com';
+$gmailpassword = 'Domenico7!'; // App password for Gmail SMTP : moyv xjxf uivr hhcg 
+$gmailusername = 'knockoutnationselite';
 
 ?>
